da3ded381778542885fe992d1f5178fb70c52386
kurubushi <krbshi@gmail.com>
2016-05-27 16:35:13 +0900

    make: prototype bot
    なんとなーく，それとなーく，これはこれでうごけるじゃんけ！

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+import qualified Data.Text as Text
+import Bot.Run (botMainLoop)
+import Bot.Config
+import Bot.Options
 
 main :: IO ()
-main = someFunc
+main = do
+  bo <- getBotOptions
+  botMainLoop
+    (Text.unpack . head $ defaultSnapshotsURLs)
+    (optJsonURL bo)
--- src/Bot/Types.hs
+++ src/Bot/Types.hs
@@ -1,1 +1,47 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
 module Bot.Types where
+
+import Data.Aeson
+
+type Channel = String
+type UserName = String
+type IconEmoji = String
+type URL = String
+type LTSVersion = String
+
+
+data SlackMessage = SlackMessage {
+  getChannel :: Channel
+, getUserName :: UserName
+, getText :: String
+, getIconEmoji :: IconEmoji
+} deriving (Show, Eq)
+
+instance ToJSON SlackMessage where
+  toJSON sm = object ["channel" .= getChannel sm,
+                      "username" .= getUserName sm,
+                      "text" .= getText sm,
+                      "icon_emoji" .= getIconEmoji sm]
+
+
+data LTSInfo = LTSInfo {
+  gLTS :: LTSVersion
+, gLTS2 :: LTSVersion
+, gLTS3 :: LTSVersion
+, gLTS4 :: LTSVersion
+, gLTS5 :: LTSVersion
+, gLTS6 :: LTSVersion
+}
+
+instance FromJSON LTSInfo where
+  parseJSON = withObject "ltsinfo" $ \o -> do
+    gLTS  <- o .: "lts"
+    gLTS2 <- o .: "lts-2"
+    gLTS3 <- o .: "lts-3"
+    gLTS4 <- o .: "lts-4"
+    gLTS5 <- o .: "lts-5"
+    gLTS6 <- o .: "lts-6"
+    return LTSInfo{..}
+
