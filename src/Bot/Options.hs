451e48d2b7f66ef1e9c089039de3148cf680a67d
kurubushi <krbshi@gmail.com>
2016-05-27 16:43:21 +0900

    add: option parser
    おぷしょん，ぱーさーを，やったぞ！

---
+++ src/Bot/Options.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE RecordWildCards #-}
+
+module Bot.Options (
+  BotOptions(..)
+, getBotOptions
+) where
+
+import Bot.Types
+import Options.Applicative
+
+data BotOptions = BotOptions {
+  optJsonURL :: URL
+}
+
+botOptionsParser :: Parser BotOptions
+botOptionsParser = BotOptions
+  <$> argument str (metavar "URL")
+
+allBotInfo :: ParserInfo BotOptions
+allBotInfo = info
+  (helper <*> botOptionsParser)
+  (fullDesc
+    <> progDesc "LTSHaskell Slack Bot"
+    <> header   "notification updating of LTSHaskell")
+
+getBotOptions :: IO BotOptions
+getBotOptions = execParser allBotInfo
